ea61c9b2e265a93cbb21a4991e9639c5b5cd5886
NicoNekoru <66227158+NicoNekoru@users.noreply.github.com>
2026-07-20 05:50:08 -0700

    Update tekai to 0.2.0

--- Formula/tekai.rb
+++ Formula/tekai.rb
@@ -1,8 +1,8 @@
 class Tekai < Formula
   desc "Self-contained, fidelity-preserving LaTeX engine and build system"
   homepage "https://github.com/NicoNekoru/tekai"
-  url "https://github.com/NicoNekoru/tekai/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "1fb557a6b14f501b41dcc4337ae1dfcb46091d0dd5f6be0c514e3ffeb64d6989"
+  url "https://github.com/NicoNekoru/tekai/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "e018ddd98dd39538f6a48a27bccf88ecd42f7d9cd35545392818968b1250ddcc"
   license "MIT"
   head "https://github.com/NicoNekoru/tekai.git", branch: "main"
 
